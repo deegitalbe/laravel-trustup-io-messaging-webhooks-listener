eed5e1460f2d93fc2442cb6689bb265828a2c357
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-02-24 17:36:24 +0100

    :loud_sound: 🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️

--- src/Http/Controllers/Webhooks/Messages/MessageCreatedController.php
+++ src/Http/Controllers/Webhooks/Messages/MessageCreatedController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Deegitalbe\LaravelTrustupIoMessagingWebhooksListeners\Contracts\Listeners\Guards\AppKeyListenerGuardContract;
 use Deegitalbe\LaravelTrustupIoMessagingWebhooksListeners\Contracts\Listeners\Messages\TrustupIoMessagingMessageCreatedListenerContract;
+use Illuminate\Support\Facades\Log;
 
 class MessageCreatedController extends Controller
 {
@@ -13,7 +14,17 @@ class MessageCreatedController extends Controller
         TrustupIoMessagingMessageCreatedListenerContract $listener,
         AppKeyListenerGuardContract $guard
     ) {
+        Log::error("this is before should trigger", [
+            "request" => $request->all(),
+            "app_name" => $request->input('conversation.app_name'),
+            "listener" => $listener::class
+        ]);
+
         $shouldTrigger = $guard->shouldTrigger($listener, $request->input('conversation.app_name'));
+
+        Log::error("should trigger value", [
+            "shouldTrigger" => $shouldTrigger
+        ]);
         
         if (!$shouldTrigger) return;
 
